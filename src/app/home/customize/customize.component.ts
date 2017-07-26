f66a78ae4214929e136f4ada617a7fca0879217a
reshmaj <reshmaj@qburst.com>
2017-07-26 17:31:04 +0530

    expand details feature added

--- src/app/home/customize/customize.component.ts
+++ src/app/home/customize/customize.component.ts
@@ -9,7 +9,20 @@ export class CustomizeComponent implements OnInit {
 
   constructor() { }
 
+  public firstItemClicked: boolean = false;
+  public secondItemClicked: boolean = false;
+
   ngOnInit() {
+  }
+
+  public expandItem(itemNumber: number) {
+    if (itemNumber === 1) {
+      this.firstItemClicked = !this.firstItemClicked;
+    } else if(itemNumber === 2) {
+      this.secondItemClicked = !this.secondItemClicked;
+    }
+    console.log('insider');
+    
   }
 
 }
